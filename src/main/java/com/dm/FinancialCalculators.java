6d9068fef88e76ac0277fe6155f8f2e7577eb843
Demetrius <Dmayfield@appdev.yearup.or>
2023-04-14 13:59:19 -0400

    First commit

---
+++ src/main/java/com/dm/FinancialCalculators.java
@@ -0,0 +1,36 @@
+package com.dm;
+
+import java.util.Scanner;
+public class FinancialCalculators {
+
+    public  static void main (String[] args){
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println( "What is the principle: " );
+        float principleRes = myScanner.nextFloat(); // Principle answer
+
+        System.out.println(""); // Paragraph spacing
+
+        System.out.println("Please input the interest rate: ");
+        float interestRes = myScanner.nextFloat(); // Interest rate answer
+
+        System.out.println(""); // Paragraph spacing
+
+        System.out.println("What is the length of the loan in months: "); // Length of the loan answer
+        float lengthRes = myScanner.nextFloat();
+
+        System.out.println(""); // Paragraph spacing
+
+//        float principle = principleRes;
+//        float interst = interestRes;
+//        float length = lengthRes;
+
+
+
+
+//        int principal =
+//        int interestRate =
+//        int loanLength =
+    }
+}
