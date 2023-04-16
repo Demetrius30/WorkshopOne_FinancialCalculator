e6291822b8ad3e5c65288403e886b5fa6ebb0506
Demetrius <Dmayfield@appdev.yearup.or>
2023-04-16 13:10:54 -0400

    Second commit

--- src/main/java/com/dm/FinancialCalculators.java
+++ src/main/java/com/dm/FinancialCalculators.java
@@ -9,18 +9,18 @@ public class FinancialCalculators {
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("What is the principal: ");
-        double principalRes = myScanner.nextFloat(); // Principal response
+        double principalRes = myScanner.nextDouble(); // Principal response
 
 
         System.out.println("What is the interest rate: ");
-        double interestRes = myScanner.nextFloat(); // Interest rate response
+        float interestRes = myScanner.nextFloat(); // Interest rate response
 
 
         System.out.println("What is the length of the loan in years: "); // loan length response in years
-        double loanLengthRes = myScanner.nextFloat();
+        double loanLengthRes = myScanner.nextDouble();
 
         // Mortgage calculation
-        double monthlyPayment = (principalRes * interestRes / loanLengthRes);
+        double monthlyPayment = (principalRes * ( interestRes/12) / (1.0 - (Math.pow(1.0 + interestRes/12, - loanLengthRes))));
 
         // Interest rate calculation
         double totalInterestPaid = (monthlyPayment * loanLengthRes) - principalRes;
