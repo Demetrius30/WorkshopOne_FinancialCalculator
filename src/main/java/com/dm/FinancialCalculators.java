d766d0ba3d0150ba7ea756558469c647778b9e9a
Demetrius <Dmayfield@appdev.yearup.or>
2023-04-16 13:02:13 -0400

    Second commit

--- src/main/java/com/dm/FinancialCalculators.java
+++ src/main/java/com/dm/FinancialCalculators.java
@@ -1,36 +1,63 @@
 package com.dm;
 
+import java.security.Principal;
 import java.util.Scanner;
 public class FinancialCalculators {
 
-    public  static void main (String[] args){
+    public static void main(String[] args) {
 
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println( "What is the principle: " );
-        float principleRes = myScanner.nextFloat(); // Principle answer
+        System.out.println("What is the principal: ");
+        double principalRes = myScanner.nextFloat(); // Principal response
 
-        System.out.println(""); // Paragraph spacing
 
-        System.out.println("Please input the interest rate: ");
-        float interestRes = myScanner.nextFloat(); // Interest rate answer
+        System.out.println("What is the interest rate: ");
+        double interestRes = myScanner.nextFloat(); // Interest rate response
 
-        System.out.println(""); // Paragraph spacing
 
-        System.out.println("What is the length of the loan in months: "); // Length of the loan answer
-        float lengthRes = myScanner.nextFloat();
+        System.out.println("What is the length of the loan in years: "); // loan length response in years
+        double loanLengthRes = myScanner.nextFloat();
 
-        System.out.println(""); // Paragraph spacing
+        // Mortgage calculation
+        double monthlyPayment = (principalRes * interestRes / loanLengthRes);
 
-//        float principle = principleRes;
-//        float interst = interestRes;
+        // Interest rate calculation
+        double totalInterestPaid = (monthlyPayment * loanLengthRes) - principalRes;
+
+        // Total payment calculation
+        double totalPayment = monthlyPayment + totalInterestPaid;
+
+        //Output for variables above
+        System.out.printf("Your monthly payment is $%.2f" + monthlyPayment);
+        System.out.printf("Your total interest is $%.2f" + totalPayment);
+        System.out.printf("In total you pay $%.2f" + totalPayment );
+
+
+
+
+        // look up how to Calculate Mortgage payments
+
+//        Principal = $55,000
+//        Interest = 7.625%
+//        Loan length = 10 years
+
+//        double Principal = principalRes;
+//        double Interest = interestRes;
+//        double loanLength = loanLengthRes;
+
+
+//        double sum1 = principalRes + interestRes;
+//        double sum2 =
+
+//        float principle = principalRes;
+//        float interest = interestRes;
 //        float length = lengthRes;
-
-
 
 
 //        int principal =
 //        int interestRate =
 //        int loanLength =
+
     }
 }
